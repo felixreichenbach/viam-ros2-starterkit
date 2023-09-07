ec72e4c5c75e57510bfe277de082221f8aa850bf
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-09-07 11:48:44 +0200

    utils uploaded

---
+++ src/ros2/rclpy_utils.py
@@ -0,0 +1,59 @@
+import rclpy
+from threading import Thread
+from viam.logging import getLogger
+
+
+class RclpyNodeManager:
+    """
+    RclpyNodeManager
+
+    A singleton pattern to ensure we have only
+    one Multithreaded executor spinning for the
+    viam intagration.
+
+    The multithreaded executor is used so we can
+    support multiple nodes as needed.
+    """
+    mgr = None
+
+    @classmethod
+    def get_instance(cls):
+        """
+        return either a new instance or the existing
+
+        :return:
+        """
+        if cls.mgr is None:
+            rclpy.init(args=None)
+            cls.mgr = RclpyNodeManager()
+        return cls.mgr
+
+    def __init__(self):
+        self.logger = getLogger(__name__)
+        self.logger.debug('RclpyNodeManager: initialized rclpy')
+        self.executor = rclpy.executors.MultiThreadedExecutor()
+        self.executor_thread = None
+        self.logger.debug(f'Created RclpyNodeManager {self.executor}')
+
+    def spin_and_add_node(self, node):
+        self.logger.debug(f'RclpyNodeManager: attempting to add: {node.get_name()}')
+        self.executor.add_node(node)
+
+        if self.executor_thread is None:
+            self.logger.debug('creating executor thread for multithreaded executor')
+            self.executor_thread = Thread(target=self.executor.spin, daemon=True)
+            self.executor_thread.start()
+        self.logger.debug(f'RclpyNodeManager: node({node.get_name()}) to executor')
+
+    def remove_node(self, node):
+        self.logger.debug(f'RclpyNodeManager: attempting to remove node: {node.get_name()}')
+        for n in self.executor.get_nodes():
+            if n.get_name() == node.get_name():
+                self.logger.info(f'RclpyNodeManager: found {node.get_name()}, removing node')
+                self.executor.remove_node(node)
+        self.logger.debug(f'RclpyNodeManager: successfully remove node: {node.get_name()}')
+
+    def shutdown(self):
+        self.logger.info('shutting down rclpy executor & joining threads')
+        rclpy.shutdown()
+        self.executor_thread.join()
